7553cba4f8e1d5bf907ad9068f29908804371bb0
Oscar Bucio <oscarbucio2001@gmail.com>
2022-11-25 12:28:08 -0600

    done: decorators.py

--- decorators.py
+++ decorators.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 def execution_time(func):
-    def wrapper():
+    def wrapper(*args, **kwargs): 
         initial_time = datetime.now() # Devuelve fecha y hora del momento que se ejecuta
-        func()
+        func(*args, **kwargs)
         final_time = datetime.now()
         time_elapsed = final_time - initial_time
         print(f'Pasaron {time_elapsed.total_seconds()} segundos') # Se obtienen la cantidad de segundos con el método total_seconds() 
@@ -12,7 +12,36 @@ def execution_time(func):
 # Aquí se procede a decorar
 @execution_time
 def random_func():
-    for _ in range(1,1000000):
+    for _ in range(1,100000):
         pass
 
+
+@execution_time
+def suma(a: int, b: int) -> int: 
+    return a + b
+
+
+@execution_time
+def saludo(nombre="Oscar"):
+    print(f"Hola {nombre}")
+
+
 random_func()
+suma(5,5)
+saludo("Daniel")
+
+
+# Reto
+
+def saludo(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        print("Hola, estoy colando este mensaje y estoy decorando")
+    return wrapper
+
+@saludo
+def llamadaNombre(name="Anonimo"):
+    print(f"Hola {name}, espero que no se cole el mensaje del decorador")
+
+llamadaNombre("Jose")
+print("Ay no, si se coló")

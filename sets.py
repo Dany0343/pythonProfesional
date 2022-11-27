ec167072d0369ce23099e0c7d5b7379beebb11fa
Oscar Bucio <oscarbucio2001@gmail.com>
2022-11-27 15:21:46 -0600

    done: eliminate duplicates on sets.py

--- sets.py
+++ sets.py
@@ -25,30 +25,56 @@
 
 
 # Symetric difference
-my_set1 = {3, 4, 5}
-my_set2 = {5, 6, 7}
+# my_set1 = {3, 4, 5}
+# my_set2 = {5, 6, 7}
 
-my_set3 = my_set1 ^ my_set2
-print(my_set3)
+# my_set3 = my_set1 ^ my_set2
+# print(my_set3)
 
 
 
 # Reto
-futbol = {"Daniel", "Juan", "Ana"}
-basquet = {"Daniel", "Ana", "Oscar"}
+# futbol = {"Daniel", "Juan", "Ana"}
+# basquet = {"Daniel", "Ana", "Oscar"}
 
-# Union
-todos = futbol | basquet
-print(todos)
+# # Union
+# todos = futbol | basquet
+# print(todos)
 
-# Intersección
-ambos = futbol & basquet
-print(ambos)
+# # Intersección
+# ambos = futbol & basquet
+# print(ambos)
 
-# Diferencia
-soloFutbol = futbol - basquet
-print(soloFutbol)
+# # Diferencia
+# soloFutbol = futbol - basquet
+# print(soloFutbol)
 
-# Simetrica
-unoUOtro = futbol ^ basquet
-print(unoUOtro)
+# # Simetrica
+# unoUOtro = futbol ^ basquet
+# print(unoUOtro)
+
+
+# Eliminar duplicados
+def removeDuplicates(some_list):
+    withoutDuplicates = []
+
+    for element in some_list:
+        if element not in withoutDuplicates: # Si el elemento no está en la lista
+            withoutDuplicates.append(element)
+
+    return withoutDuplicates
+
+
+# Otra forma más rápida
+def removeDuplicates2(some_list):
+    return list(set(some_list))
+
+
+def run():
+    random_list = [1, 1, 2, 4]
+    print(removeDuplicates(random_list))
+    print(removeDuplicates2(random_list))
+
+
+if __name__ == '__main__':
+    run()

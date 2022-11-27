1c1bf272c151a9ddbc88ce9126e9963b12866b16
Oscar Bucio <oscarbucio2001@gmail.com>
2022-11-27 14:51:45 -0600

    done: sets.py

---
+++ sets.py
@@ -0,0 +1,32 @@
+# my_set1 = {3, 4, 5}
+# my_set2 = {5, 6, 7}
+
+# my_set3 = my_set1 | my_set2
+# print(my_set3)
+
+
+# Intersection
+# my_set1 = {3, 4, 5}
+# my_set2 = {5, 6, 7}
+
+# my_set3 = my_set1 & my_set2
+# print(my_set3)
+
+
+# Difference
+# my_set1 = {3, 4, 5}
+# my_set2 = {5, 6, 7}
+
+# my_set3 = my_set1 - my_set2
+# print(my_set3)
+
+# my_set4 = my_set2 - my_set1
+# print(my_set4)
+
+
+# Simetric difference
+my_set1 = {3, 4, 5}
+my_set2 = {5, 6, 7}
+
+my_set3 = my_set1 ^ my_set2
+print(my_set3)

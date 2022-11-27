fceb7171a94825e04c9e20a18e56c16684de157b
Oscar Bucio <oscarbucio2001@gmail.com>
2022-11-26 21:35:54 -0600

    done: iterators.py

--- iterators.py
+++ iterators.py
@@ -2,6 +2,9 @@ import time
 
 class FiboIter():
 
+    def __init__(self, max=None):
+        self.max = max
+
     def __iter__(self):
         self.n1 = 0
         self.n2 = 1
@@ -9,25 +12,29 @@ class FiboIter():
         return self # Retorna al objeto en si mismo
 
     def __next__(self):
-        if self.counter == 0:
-            self.counter+= 1
-            return self.n1
-        elif self.counter == 1:
-            self.counter += 1
-            return self.n2
+        if not self.max or (self.n1 + self.n2) <= self.max :
+            if self.counter == 0:
+                self.counter+= 1
+                return self.n1
+            elif self.counter == 1:
+                self.counter += 1
+                return self.n2
+            else:
+                self.aux = self.n1 + self.n2
+                # self.n1 = self.n2
+                # self.n2 = self.aux
+                self.n1, self.n2 = self.n2, self.aux # Es lo mismo de arriba pero de forma resumida, se hace un swapping
+                self.counter += 1
+                return self.aux
         else:
-            self.aux = self.n1 + self.n2
-            # self.n1 = self.n2
-            # self.n2 = self.aux
-            self.n1, self.n2 = self.n2, self.aux # Es lo mismo de arriba pero de forma resumida, se hace un swapping
-            self.counter += 1
-            return self.aux
+            raise StopIteration
 
 
 if __name__ == '__main__':
     # Se instancia la clase
     # Instanciar es el acto de convertir a partir de los planos de una clase a un objeto
-    fibonacci = FiboIter()
-    for element in fibonacci:
+    max = int(input("Introduce el numero maximo hasta donde va a parar: "))
+    fibonacci = FiboIter(max)
+    for element in fibonacci: # El for se utiliza ya que como habíamos platicado es azucar sintactica para un while que utiliza iter
         print(element)
-        time.sleep(0.05)
+        time.sleep(0.05) # Se pausa para ver la sucesión de forma lenta
